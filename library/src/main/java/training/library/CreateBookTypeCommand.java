71c940ad7407de5f7707fe6e2b87aee70566ca0e
VLevv <vadaslevi@gmail.com>
2022-06-20 00:51:47 +0200

    create commands

---
+++ library/src/main/java/training/library/CreateBookTypeCommand.java
@@ -0,0 +1,24 @@
+package training.library;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateBookTypeCommand {
+    @Schema(description = "title of the book",example = "Vuk")
+    @NotBlank
+    private String bookTitle;
+
+    @Schema(description = "the amount of books on hand",example = "12")
+    @NotNull
+    @Min(0)
+    private int amount;
+}
---
+++ library/src/main/java/training/library/CreatePersonCommand.java
@@ -0,0 +1,34 @@
+package training.library;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePersonCommand {
+
+    @Schema(description = "name of person",example = "John Doe")
+    @NotBlank
+    private String name;
+
+    @Schema(description = "date of birth",example = "2000-01-01")
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @Schema(description = "books in person's possession")
+    @NotNull
+    @OneToMany(mappedBy = "person")
+    private List<Book> books = new ArrayList<>();
+}
