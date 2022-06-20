c547e04f16db452ec4fc4d02cd4c655505cccefa
VLevv <vadaslevi@gmail.com>
2022-06-20 11:49:04 +0200

    book controller

---
+++ library/src/main/java/training/library/BookController.java
@@ -0,0 +1,52 @@
+package training.library;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/books")
+@Tag(name = "Operations on Book")
+public class BookController {
+    private BookService service;
+
+    @GetMapping("/find-all-books")
+    public List<BookDto> findAllBooks(Optional<String> title){
+        return service.findAllBooks(title);
+    }
+
+    @GetMapping("/find-book/{id}")
+    public BookDto findBookById(@PathVariable long id){
+        return service.findBookById(id);
+    }
+
+    @PostMapping("/create-new-book")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@Valid CreateBookCommand command){
+        return service.createBook(command);
+    }
+
+    @PutMapping("/update-time-of-return/{id}")
+    public BookDto updateTimeOfReturn(@PathVariable long id){
+        return service.updateTimeOfReturn(id);
+    }
+
+    @DeleteMapping("/remove-book/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBookById(@PathVariable long id){
+        service.removeBookById(id);
+    }
+
+    @DeleteMapping("/remove-books")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBooks(){
+        service.removeBooks();
+    }
+
+}
